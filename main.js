d8524a81302617710719bb36254a5ec842cc0cce
TranTruong16 <lucyen11111@gmail.com>
2019-02-28 23:07:12 +0700

    btap

--- main.js
+++ main.js
@@ -1,1 +1,25 @@
-console.log("hello");
+function checkNumber(n) {
+    if (n < 2) {
+        return false;
+    }
+    for (let i = 2; i < (n / 2); i++) {
+        if (n % i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+function showResult() {
+    var value = document.getElementById("number").value;
+    if (isNaN(value)) {
+        alert("Nhap lai");
+        document.getElementById("number").value = "";
+        document.getElementById("number").focus();
+    } else {
+        if (checkNumber(value)) {
+            alert('day la so nguyen to');
+        } else {
+            alert("khong phai so nguyen to");
+        }
+    }
+}
